476cf71caaf1910202802b309c248ebb337ae74b
SazidHasan2003 <sarkersazid123@gmail.com>
2026-03-09 23:48:40 +0600

    selecting element with js

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("loginForm");
+const loginPage = document.getElementById("loginPage");
+const dashboardPage = document.getElementById("dashboardPage");
+const issuesGrid = document.getElementById("issuesGrid");
+const searchInput = document.querySelector(
+  "input[placeholder='Search issues...']",
+);
+const loader = document.getElementById("loader");
+// Modal
